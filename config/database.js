61d00503b5f7938a16542991a2d0f99ed951b5a4
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-10-21 01:25:01 -0200

    Config database

--- config/database.js
+++ config/database.js
@@ -1,3 +1,8 @@
 module.exports = {
-  'url': 'your-settings-here' // mongodb settings
+  'url': process.env.MONGODB_URI // mongodb settings
 };
+
+// mongoose.Promise = global.Promise;
+// mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/todolist',{
+//   useMongoClient: true,
+// }); 
