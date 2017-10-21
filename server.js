a031e07db442bdb8cc31af88cadc2c0ebe6be810
Luiz Felippe Gonçalves <felippedev@hotmail.com>
2017-10-21 01:14:51 -0200

    Second commit, all files in

---
+++ server.js
@@ -0,0 +1,29 @@
+let express  = require('express');
+let app      = express();
+let port     = process.env.PORT || 8080;
+let mongoose = require('mongoose');
+let passport = require('passport');
+let flash    = require('connect-flash');
+
+let morgan       = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser   = require('body-parser');
+let session      = require('express-session');
+
+let configDB = require('./config/database.js');
+
+app.use(morgan('dev')); // log every request to the console
+app.use(cookieParser()); // read cookies (needed for auth)
+app.use(bodyParser()); // get information from html forms
+app.use(session({ secret: 'ilovescotchscotchyscotchscotch' })); // session secret
+app.use(passport.initialize());
+app.use(passport.session()); // persistent login sessions
+app.use(flash()); // use connect-flash for flash messages stored in session
+
+app.set('view engine', 'ejs'); // set up ejs for templating
+
+require('./app/routes.js')(app, passport);
+require('./config/passport')(passport); // pass passport for configuration
+
+app.listen(port);
+console.log('Server listening on port ' + port);
